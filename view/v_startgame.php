90b40fd130925da45d44198dc211bd63bcc3350f
Igor <iva_co@ukr.net>
2017-11-19 10:00:31 +0200

    timer and flip back fix
    Added timer
    Fixed flip back tiles even if match

--- view/v_startgame.php
+++ view/v_startgame.php
@@ -1,22 +1,15 @@
 <div id="field">
 
-  <?php $j = 1; ?>
-<!--  <div id="row--><?//=$j?><!--">-->
   <div id="fieldContainer">
     <?php
-      for ($i = 0; $i < $field_size * 2; $i++) :
-  //      if ($i > 0 && $i % 4 == 0) : ?>
-  <!--        </div>-->
-  <!--        <div id="row--><?//=++$j?><!--">-->
-  <!--      --><?php //endif; ?>
+      for ($i = 0; $i < $field_size * 2; $i++) : ?>
         <div class="cards">
-          <div id='card-<?=$i+1?>' class="front card"></div>
-          <div class="back card card<?=$numbers[$i]?>" data-tile="<?=$numbers[$i]?>"></div>
+          <div id='card-<?=$i+1?>' class="front card"><?=$numbers[$i]?></div>
+          <div id='back-<?=$i+1?>' class="back card card<?=$numbers[$i]?>" data-tile="<?=$numbers[$i]?>"></div>
         </div>
       <?php
       endfor;
     ?>
-  <!--  </div>-->
 
     <input id="player_id" type="hidden" value="<?=$player_id?>">
     <input id="field_size" type="hidden" value="<?=$field_size?>">
