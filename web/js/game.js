730e94e4cb9e4aee9a670de3f610e67553182f52
igorv <ivoyt90@gmail.com>
2018-08-11 16:32:44 +0300

    rework to use async xhr

--- web/js/game.js
+++ web/js/game.js
@@ -4,49 +4,63 @@
 function startGame () {
     var sendPlayerName  = document.getElementById('enter-player-name').value;
     var fieldSize       = document.querySelector('.select-field-size-selected');
-    var postBody        = 'player_name=' + sendPlayerName + '&field_size=' + fieldSize.dataset.fieldsize;
+    var fieldSizeData = '';
+    if (fieldSize !== null) {
+        fieldSizeData = fieldSize.dataset.fieldsize;
+    }
+    var postBody        = 'player_name=' + sendPlayerName + '&field_size=' + fieldSizeData;
     var ajaxStartGame   = new XMLHttpRequest();
 
-    ajaxStartGame.open('POST', '/game/start', false);
+    ajaxStartGame.timeout = 3000;
+    ajaxStartGame.onreadystatechange = function () {
+        if (ajaxStartGame.readyState === 4) {
+            if ( ajaxStartGame.status !== 200 ) {
+                alert(ajaxStartGame.status + ': ' + ajaxStartGame.statusText);
+                console.log(ajaxStartGame.statusText);
+            } else {
+                var content     = document.getElementById('content');
+                var error_block = document.getElementById('error-msg');
+                try {
+                    var json = JSON.parse(ajaxStartGame.responseText);
+                    console.log(ajaxStartGame.responseText);
+                    console.log(json);
+                    if (json.err === 1) {
+                        error_block.innerHTML = json.response;
+                        return;
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+                error_block.innerHTML       = '';
+                start_block.style.opacity   = 0;
+                document.body.style.height  = '100vh';
+
+                var playerName = document.getElementById('player-name');
+
+                setTimeout(function() {
+                    start_block.remove();
+                    content.innerHTML = ajaxStartGame.responseText;
+                    window.addEventListener("resize", calculateFieldSize);
+                    setTimeout(function ()  {
+                        var field                       = document.getElementById('field');
+                        field.style.opacity             = 1;
+                        gameTriggerBtnText.innerHTML    = buttonText['pause'];
+                        gameTriggerBtn.style.opacity    = 1;
+                        playerName.innerHTML            = sendPlayerName;
+                        calculateFieldSize();
+                        startTimer();
+                    }, 300);
+                }, 800);
+            }
+        }
+    };
+    ajaxStartGame.ontimeout = function () {
+        alert('Waiting time expired. Please try again later...');
+    };
+
+    ajaxStartGame.open('POST', '/game/start', true);
     ajaxStartGame.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     ajaxStartGame.send(postBody);
-
-    if ( ajaxStartGame.status !== 200 ) {
-        alert(ajaxStartGame.status + ': ' + ajaxStartGame.statusText);
-        console.log(ajaxStartGame.statusText);
-    } else {
-        var content     = document.getElementById('content');
-        var error_block = document.getElementById('error-msg');
-        try {
-            var json = JSON.parse(ajaxStartGame.responseText);
-            if (json.err === 1) {
-                error_block.innerHTML = json.response;
-                return;
-            }
-        } catch (e) {
-            console.log(e);
-        }
-        error_block.innerHTML       = '';
-        start_block.style.opacity   = 0;
-        document.body.style.height  = '100vh';
-
-        var playerName = document.getElementById('player-name');
-
-        setTimeout(function() {
-            start_block.remove();
-            content.innerHTML = ajaxStartGame.responseText;
-            window.addEventListener("resize", calculateFieldSize);
-            setTimeout(function ()  {
-                var field                       = document.getElementById('field');
-                field.style.opacity             = 1;
-                gameTriggerBtnText.innerHTML    = buttonText['pause'];
-                gameTriggerBtn.style.opacity    = 1;
-                playerName.innerHTML            = sendPlayerName;
-                calculateFieldSize();
-                startTimer();
-            }, 300);
-        }, 800);
-    }
 }
 
 /**
@@ -56,31 +70,38 @@ function restartGame() {
     var ajaxReStartGame = new XMLHttpRequest();
     var postBody = 'restart=1';
 
-    ajaxReStartGame.open('POST', '/', false);
+    ajaxReStartGame.timeout = 3000;
+    ajaxReStartGame.onreadystatechange = function () {
+        if (ajaxReStartGame.readyState === 4) {
+            if ( ajaxReStartGame.status !== 200 ) {
+                alert(ajaxReStartGame.status + ': ' + ajaxReStartGame.statusText);
+            } else {
+                var content = document.getElementById('content');
+                content.style.opacity = 0;
+                setTimeout(function() {
+                    content.innerHTML = ajaxReStartGame.responseText;
+                    window.addEventListener("resize", calculateFieldSize);
+                    checkFieldLoaded();
+                    setTimeout(function ()  {
+                        var field = document.getElementById('field');
+                        content.style.opacity = 1;
+                    }, 100);
+                }, 700);
+
+                gameTriggerBtn.removeEventListener('click', restartGame, false);
+                gameTriggerBtn.style.opacity = 0;
+                gameInfoInit();
+                clearTimer();
+                startTimer();
+            }
+        }
+    };
+    ajaxReStartGame.ontimeout = function () {
+        alert('Waiting time expired. Please try again later...');
+    };
+    ajaxReStartGame.open('POST', '/', true);
     ajaxReStartGame.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     ajaxReStartGame.send(postBody);
-
-    if ( ajaxReStartGame.status !== 200 ) {
-        alert(ajaxReStartGame.status + ': ' + ajaxReStartGame.statusText);
-    } else {
-        var content = document.getElementById('content');
-        content.style.opacity = 0;
-        setTimeout(function() {
-            content.innerHTML = ajaxReStartGame.responseText;
-            window.addEventListener("resize", calculateFieldSize);
-            checkFieldLoaded();
-            setTimeout(function ()  {
-                var field = document.getElementById('field');
-                content.style.opacity = 1;
-            }, 100);
-        }, 700);
-
-        gameTriggerBtn.removeEventListener('click', restartGame, false);
-        gameTriggerBtn.style.opacity = 0;
-        gameInfoInit();
-        clearTimer();
-        startTimer();
-    }
 }
 
 /**
@@ -105,9 +126,7 @@ function scoring() {
             } else {
                 score += (100 - tileOpenTime);
             }
-        }
-        //else if (lastTrie <= 9) {
-        else {
+        } else {
             if ((100 - lastTrie * 10) - tileOpenTime <= 0) {
                 score += 1;
             } else {
